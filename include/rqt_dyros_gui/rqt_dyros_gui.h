a7cf5dd6db20ac3060834e1b0aecab0df08a7a11
Lee Hokyun <68094299+Hokyun-Lee@users.noreply.github.com>
2024-01-30 02:00:03 +0900

    Add files via upload

---
+++ include/rqt_dyros_gui/rqt_dyros_gui.h
@@ -0,0 +1,43 @@
+#ifndef RQT_DYROS_H
+#define RQT_DYROS_H
+
+#include <rqt_gui_cpp/rqt_gui_cpp/plugin.h>
+#include <ui_rqt_dyros_gui.h>
+#include <QWidget>
+#include <std_msgs/msg/float32.hpp>
+#include <iostream>
+
+namespace rqt_dyros_gui {
+
+class RqtDyrosPlugin
+  : public rqt_gui_cpp::Plugin
+{
+  Q_OBJECT
+public:
+  RqtDyrosPlugin();
+  virtual void initPlugin(qt_gui_cpp::PluginContext& context);
+  virtual void shutdownPlugin();
+  virtual void saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const;
+  virtual void restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& instance_settings);
+
+  void subCallback(const std_msgs::msg::Float32::SharedPtr msg) const;
+
+
+protected slots:
+  virtual void pushbutton();
+
+//signals:
+  // Comment in to signal that the plugin has a way to configure it
+  //bool hasConfiguration() const;
+  //void triggerConfiguration();
+
+private:
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr myPub;
+  rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr mySub;
+
+  Ui::RqtDyrosGuiWidget ui_;
+  QWidget* widget_;
+};
+} // namespace
+
+#endif // my_namespace__my_plugin_H
