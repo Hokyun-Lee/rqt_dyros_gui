9e61b76cd075f959d6bc7e07e16c0ea68415f05a
Hokyun-Lee <lhg0805@gmail.com>
2024-02-06 01:55:57 +0900

    maybe final commit

--- include/rqt_dyros_gui/rqt_dyros_gui.h
+++ include/rqt_dyros_gui/rqt_dyros_gui.h
@@ -4,8 +4,17 @@
 #include <rqt_gui_cpp/rqt_gui_cpp/plugin.h>
 #include <ui_rqt_dyros_gui.h>
 #include <QWidget>
-#include <std_msgs/msg/float32.hpp>
+
+#include "std_msgs/msg/bool.hpp"
+#include "std_msgs/msg/float32.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/float32_multi_array.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
 #include <iostream>
+
+#define PI 3.14159265359
+#define deg2rad(deg)  ((deg) * PI / 180.0)
+#define rad2deg(rad)  ((rad) * 180.0 / PI)
 
 namespace rqt_dyros_gui {
 
@@ -15,16 +24,22 @@ class RqtDyrosPlugin
   Q_OBJECT
 public:
   RqtDyrosPlugin();
+  int TOTALDOF;
   virtual void initPlugin(qt_gui_cpp::PluginContext& context);
   virtual void shutdownPlugin();
   virtual void saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const;
   virtual void restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& instance_settings);
 
-  void subCallback(const std_msgs::msg::Float32::SharedPtr msg) const;
-
+  void subCallback(const sensor_msgs::msg::JointState msg);
+  void realSubCallback(const sensor_msgs::msg::JointState msg);
+  
 
 protected slots:
-  virtual void pushbutton();
+  virtual void sendbutton();
+  virtual void sendinitbutton();
+  virtual void sendemstop();
+  virtual void sendstart();
+
 
 //signals:
   // Comment in to signal that the plugin has a way to configure it
@@ -32,9 +47,11 @@ protected slots:
   //void triggerConfiguration();
 
 private:
-  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr myPub;
-  rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr mySub;
-
+  rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr jointstates_Sub;
+  rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr jointstatesReal_Sub;
+  rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr myParam;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr myCommand;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr exec_pub_;
   Ui::RqtDyrosGuiWidget ui_;
   QWidget* widget_;
 };
--- src/rqt_dyros_gui.cpp
+++ src/rqt_dyros_gui.cpp
@@ -8,6 +8,7 @@ namespace rqt_dyros_gui
   RqtDyrosPlugin::RqtDyrosPlugin()
       : rqt_gui_cpp::Plugin(), widget_(0)
   {
+    TOTALDOF = 6;
     // Constructor is called first before initPlugin function, needless to say.
 
     // give QObjects reasonable names
@@ -24,26 +25,88 @@ namespace rqt_dyros_gui
     ui_.setupUi(widget_);
     // add widget to the user interface
     context.addWidget(widget_);
+    bool real_robot = true;
+    // rclcpp::Parameter::get_value("real", real_robot);
+    // mySub = node_->create_subscription<std_msgs::msg::Float32>("sub", 100, std::bind(&RqtDyrosPlugin::subCallback, this, std::placeholders::_1));
+    if(real_robot == true)
+    {
+      jointstatesReal_Sub = node_->create_subscription<sensor_msgs::msg::JointState>("motor/joint_states", 100, std::bind(&RqtDyrosPlugin::realSubCallback, this, std::placeholders::_1));
+    }
+    else
+    {
+      jointstates_Sub = node_->create_subscription<sensor_msgs::msg::JointState>("joint_states", 100, std::bind(&RqtDyrosPlugin::subCallback, this, std::placeholders::_1));
+    }
+  
+    connect(ui_.SendButton, SIGNAL(clicked()), this, SLOT(sendbutton()));
+    connect(ui_.SendInitButton, SIGNAL(clicked()), this, SLOT(sendinitbutton()));
+    connect(ui_.EMSTOP, SIGNAL(clicked()), this, SLOT(sendemstop()));
+    connect(ui_.Start, SIGNAL(clicked()), this, SLOT(sendstart()));
 
-
-    myPub = node_->create_publisher<std_msgs::msg::Float32>("pub", 100);
-    mySub = node_->create_subscription<std_msgs::msg::Float32>("sub", 100, std::bind(&RqtDyrosPlugin::subCallback, this, std::placeholders::_1));
-
-    connect(ui_.myPushButton, SIGNAL(clicked()), this, SLOT(pushbutton()));
+    myParam = node_->create_publisher<std_msgs::msg::Float32MultiArray>("guiparam", 1);
+    myCommand = node_->create_publisher<std_msgs::msg::String>("commanding", 1);
+    exec_pub_ = node_->create_publisher<std_msgs::msg::Float32>("exec_time", 1);
   }
 
-  void RqtDyrosPlugin::pushbutton()
+void RqtDyrosPlugin::sendbutton()
   {
-    auto message = std_msgs::msg::Float32();
-    message.data = ui_.lineEdit->text().toFloat();
-    
-    myPub->publish(message);
+    std_msgs::msg::Float32MultiArray data_msg;
+    std_msgs::msg::Float32 time_msg;
+
+    data_msg.data.push_back(0);
+    data_msg.data.push_back(0);
+    data_msg.data.push_back(0);
+    data_msg.data[0] = ui_.lineEdit_6->text().toFloat();
+    data_msg.data[1] = ui_.lineEdit_7->text().toFloat();
+    data_msg.data[2] = ui_.lineEdit_8->text().toFloat();
+    time_msg.data = ui_.Exec_Time->text().toFloat();
+
+    myParam->publish(data_msg);
+    exec_pub_->publish(time_msg);
   }
 
-  void RqtDyrosPlugin::subCallback(const std_msgs::msg::Float32::SharedPtr msg) const
+  void RqtDyrosPlugin::sendinitbutton()
   {
-    ui_.myLabel->setText(QString::number(msg->data));
+    std_msgs::msg::String msg;
+    msg.data = "initpose";
+    myCommand->publish(msg);
   }
+
+  void RqtDyrosPlugin::sendemstop()
+  {
+    std_msgs::msg::String msg;
+    msg.data = "em";
+    myCommand->publish(msg);
+  }
+
+  void RqtDyrosPlugin::sendstart()
+  {
+    std_msgs::msg::String msg;
+    msg.data = "start";
+    myCommand->publish(msg);
+  }
+
+  void RqtDyrosPlugin::subCallback(const sensor_msgs::msg::JointState msg)
+  {
+    // std::cout << "subcallback" << std::endl;
+    ui_.lineEdit_0->setText(QString::number(rad2deg(msg.position[0])));
+    ui_.lineEdit_1->setText(QString::number(rad2deg(msg.position[1])));
+    ui_.lineEdit_2->setText(QString::number(rad2deg(msg.position[2])));
+    ui_.lineEdit_3->setText(QString::number(rad2deg(msg.position[3])));
+    ui_.lineEdit_4->setText(QString::number(rad2deg(msg.position[4])));
+    ui_.lineEdit_5->setText(QString::number(rad2deg(msg.position[5])));
+  }
+
+  void RqtDyrosPlugin::realSubCallback(const sensor_msgs::msg::JointState msg)
+  {
+    // std::cout << "subcallback" << std::endl;
+    ui_.lineEdit_0->setText(QString::number(rad2deg(msg.position[0])));
+    ui_.lineEdit_1->setText(QString::number(rad2deg(msg.position[1])));
+    ui_.lineEdit_2->setText(QString::number(rad2deg(msg.position[2])));
+    ui_.lineEdit_3->setText(QString::number(rad2deg(msg.position[3])));
+    ui_.lineEdit_4->setText(QString::number(rad2deg(msg.position[4])));
+    ui_.lineEdit_5->setText(QString::number(rad2deg(msg.position[5])));
+  }
+
 
   void RqtDyrosPlugin::shutdownPlugin()
   {
